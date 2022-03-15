29f07f5490e623911268b643cffaa7da27bc1672
KL Belgrad / Savic <Aleksandar.Savic@klingspor.de>
2022-03-15 16:45:00 +0100

    first

---
+++ src/LeftSidebar.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+import "./LeftSidebar.css"
+
+function LeftSidebar (props: any) {
+    return (
+        <React.Fragment>
+            
+            <h4>{props.title}</h4>
+            <p>Test test test sdasdasda</p>
+        </React.Fragment>
+    )
+}
+
+export default LeftSidebar
