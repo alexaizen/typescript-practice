1969140f1340802a90b5af2e41c7a71b281900a4
KL Belgrad / Savic <Aleksandar.Savic@klingspor.de>
2022-03-17 16:56:06 +0100

    sf

--- src/Components/NewTask.tsx
+++ src/Components/NewTask.tsx
@@ -5,23 +5,30 @@ import "./NewTask.css"
 
 
 
-const NewTask: React.FC<{onAddTask: (title: string, desc: string ) => void}> = (props) => {
+const NewTask: React.FC<{onAddTask: ( title: string, desc: string ) => void}> = (props) => {
 
     const titleInput = useRef<HTMLInputElement>(null);
     const descInput = useRef<HTMLInputElement>(null);
 
+    
+
     const formSubmitHandler = function (e: React.FormEvent) {
     e.preventDefault();
-
+    const titleInputValue: string = titleInput.current!.value
+    const descInputValue: string = descInput.current!.value
+    props.onAddTask(titleInputValue, descInputValue)
+}
 
     return (
-    <form>
+    <form onSubmit={formSubmitHandler} >
         <label>Title</label>
         <input id="title" ref={titleInput} ></input>
         <label>Description</label>
         <input id="description" ref={descInput} ></input>
+        <button type="submit">Create Task</button>
     </form>
     )
+
 }
 
 export default NewTask;
--- src/Pages/Tasks.tsx
+++ src/Pages/Tasks.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 
 import { useState } from "react"
+import NewTask from "../Components/NewTask"
 
 import TitleBar from "../Components/TitleBar"
 import "./Tasks.css"
@@ -9,14 +10,18 @@ const Tasks = () => {
 
     const [ tasks, setTasks ] = useState<{title: string, desc: string}[]>([]);
 
-    const updateTasksList = function (title: string, desc: string) {
+    const updateTasksList = (title: string, desc: string) => {
             setTasks((prevState)=> [...prevState, {title: title, desc: desc}])
             console.log(tasks)
     }
 
     return (
         <div>
-            <TitleBar title={"Tasks"} />
+            
+            <TitleBar title={"Tasks"}/>
+            <NewTask onAddTask={updateTasksList} ></NewTask>
+            <ul>{tasks.map(task => <li>{task.title} {task.desc}</li>)}</ul>
+            
         </div>
     )
 }
